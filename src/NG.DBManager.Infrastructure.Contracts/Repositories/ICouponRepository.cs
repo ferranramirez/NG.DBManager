a74b7d22f662ba4870af6b6aaa9d69a1475b31a3
ferranramirez <ferranramireznavajon@gmail.com>
2020-07-31 12:39:39 +0200

    Add user and node in coupons

--- src/NG.DBManager.Infrastructure.Contracts/Repositories/ICouponRepository.cs
+++ src/NG.DBManager.Infrastructure.Contracts/Repositories/ICouponRepository.cs
@@ -1,10 +1,8 @@
 ﻿using NG.DBManager.Infrastructure.Contracts.Models;
-using System;
 
 namespace NG.DBManager.Infrastructure.Contracts.Repositories
 {
     public interface ICouponRepository : IRepository<Coupon>
     {
-        User GetCommerceUser(Guid NodeId);
     }
 }
---
+++ src/NG.DBManager.Infrastructure.Impl.EF/Repositories/CouponRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NG.DBManager.Infrastructure.Contracts.Models;
+using NG.DBManager.Infrastructure.Contracts.Repositories;
+using System;
+using System.Linq;
+
+namespace NG.DBManager.Infrastructure.Impl.EF.Repositories
+{
+    public class CouponRepository : Repository<Coupon>, ICouponRepository
+    {
+        public CouponRepository(DbContext context) : base(context) { }
+
+        public override Coupon Get(object id)
+        {
+            return DbSet
+                .Where(c => c.Id == (Guid)id)
+                .Include(c => c.User)
+                .Include(c => c.Node)
+                .SingleOrDefault();
+        }
+    }
+}
